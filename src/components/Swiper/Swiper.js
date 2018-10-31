f042aa0a4d9f327f2c5803b50b42d800fa8f362f
polonusg <polonusg@hnicorp.com>
2018-10-31 14:13:38 -0500

    Working Swiper

--- src/components/Swiper/Swiper.js
+++ src/components/Swiper/Swiper.js
@@ -7,13 +7,51 @@ export default class extends Component {
 
   state = {
     showingIndex: 0,
-    left: 0
+    left: 0,
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.swiperRefs = [];
   }
 
   changeLeft = (left) => {
-    this.setState({
-      left
+    this.swiperRefs.forEach((ref, i) => {
+      ref.classList.remove('move-slow');
+      ref.style.left = this.getLeft(i, left) + 'px';
     });
+  }
+
+  endSwipe = (left) => {
+    this.resetRefs();
+
+    const newState = {
+      left: 0,
+    };
+    const swipeMargin = 200;
+    if(left > window.innerWidth - swipeMargin && this.state.showingIndex - 1 > -1){
+      newState.showingIndex = this.state.showingIndex - 1;
+      this.setState(newState);
+    } else if(left < swipeMargin - window.innerWidth && this.state.showingIndex + 1 < this.props.children.length) {
+      newState.showingIndex = this.state.showingIndex + 1;
+      this.setState(newState);
+    }
+  }
+
+  saveRef = (ref, i) => {
+    this.swiperRefs[i] = ref;
+  }
+
+  resetRefs() {
+    this.swiperRefs.forEach((ref, i) => {
+      ref.classList.add('move-slow');
+      ref.style.left = this.getLeft(i) + 'px';
+    });
+  }
+
+  getLeft(i, leftOffset = 0) {
+    return (i - this.state.showingIndex) * window.innerWidth + leftOffset;
   }
 
   render() {
@@ -22,14 +60,16 @@ export default class extends Component {
       <div className="Swiper">
         {
           this.props.children.map((child, i) => {
-            // (x - showX) * 100 + '%';
-            const left = `calc(${(i - this.state.showingIndex) * 100 + '%'} - ${this.state.left})`;
+            const left = this.getLeft(i);
             return (
               <SwiperContainer
                 key={`swiper-${i}`}
                 showing={this.state.showingIndex === i}
                 left={left}
                 changeLeft={this.changeLeft}
+                endSwipe={this.endSwipe}
+                moving={this.state.moving}
+                getRef={ref => this.saveRef(ref, i)}
               >
                 {child}
               </SwiperContainer>
--- src/components/Swiper/SwiperContainer/SwiperContainer.js
+++ src/components/Swiper/SwiperContainer/SwiperContainer.js
@@ -3,46 +3,82 @@ import React, { Component } from 'react';
 import './SwiperContainer.css';
 
 
+let start = {};
+let firstMove = false;
+let latestLeft = 0;
 
-export default ({ showing, children, changeLeft, left }) => {
-  const bind = (ref) => {
-    removeListeners(ref);
-    startListening(ref);
+export default class extends Component {
+
+  bind = (ref) => {
+    this.removeListeners(ref);
+    this.startListening(ref);
+    this.props.getRef(ref);
   }
 
-  const removeListeners = (ref) => {
-    ref.removeEventListener('touchstart', touchStart);
-    ref.removeEventListener('touchmove', touchMove);
-    ref.removeEventListener('touchEnd', touchEnd);
+  removeListeners = (ref) => {
+    ref.removeEventListener('touchstart', this.touchStart);
+    ref.removeEventListener('touchmove', this.touchMove);
+    ref.removeEventListener('touchEnd', this.touchEnd);
   }
 
-  const startListening = (ref) => {
-    ref.addEventListener('touchstart', touchStart);
+  startListening = (ref) => {
+    ref.addEventListener('touchstart', this.touchStart);
   }
 
-  const touchStart = (event) => {
+  touchStart = (event) => {
     const ref = event.target;
-    // ref.removeEventListener('touchstart', touchStart);
-    ref.addEventListener('touchmove', touchMove);
-    ref.addEventListener('touchend', touchEnd);
-    console.log('START');
+    ref.addEventListener('touchmove', this.touchMove);
+    ref.addEventListener('touchend', this.touchEnd);
+
+    const pos = {
+      x: event.touches[0].clientX,
+      y: event.touches[0].clientY,
+    };
+
+    start = pos;
+    firstMove = true;
+    latestLeft = 0;
   }
 
-  const touchMove = (event) => {
-    console.log('MOVE', event);
+  touchMove = (event) => {
+    if(firstMove) {
+      const x0 = event.touches[0].clientX;
+      const y0 = event.touches[0].clientY;
+      const x1 = start.x;
+      const y1 = start.y;
+      let slope;
+      if(x1 !== x0) {
+        slope = (y1 - y0) / (x1 - x0);
+      } else {
+        slope = 1000;
+      }
+      if(slope > 1 || slope < -1) {
+        this.removeListeners(event.target);
+        firstMove = false;
+        return;
+      }
+    }
+    latestLeft = event.touches[0].clientX - start.x;
+    this.props.changeLeft(latestLeft);
   }
 
-  const touchEnd = (event) => {
-
-    console.log('END');
-    removeListeners(event.target);
+  touchEnd = (event) => {
+    this.removeListeners(event.target);
+    this.props.endSwipe(latestLeft);
   }
 
-  const Comp = () => children;
-  return (
-    <div className="SwiperContainer" ref={bind} style={{left: left}}>
-      <Comp showing={showing} />
-    </div>
-  );
+  render() {
+    const comp = React.cloneElement(this.props.children, { showing: this.props.showing });
+
+    return (
+      <div
+        className={`SwiperContainer move-slow`}
+        ref={this.bind}
+        style={{ left: this.props.left }}
+      >
+        { comp }
+      </div>
+    );
+  }
 }
 
