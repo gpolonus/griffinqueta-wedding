c6a52c0930ab410c42985ca1beb596d957820434
polonusg <polonusg@hnicorp.com>
2018-10-31 15:08:50 -0500

    StoryPage edits and mobiles stylings

--- src/Layout/StoryPage/StoryPage.js
+++ src/Layout/StoryPage/StoryPage.js
@@ -4,12 +4,16 @@ import './StoryPage.css';
 import StoryHeader from './StoryHeader/StoryHeader';
 import EngagementVideo from './EngagementVideo/EngagementVideo';
 import EngagementPic from './Engagement-0354-min-min.JPG';
-import BaldKiss from './pics/bald-kiss.jpg';
+import {
+  BaldKiss,
+  NerdsSmile,
+  StarvedRock,
+  SmileAtHome,
+} from './pics/Pics';
 import BabyPics from './BabyPics/BabyPics';
-import NerdSmile from './pics/nerds-smile.jpg'
-import StarvedRock from './pics/starved-rock.jpg'
 import FullPageImageAndText from '../../components/FullPageImageAndText/FullPageImageAndText';
 import withMobile from '../../hoc/withMobile';
+import StorySubtitle from './StorySubtitle/StorySubtitle';
 
 
 const StoryPage = ({ path, changePath, move, showing, isMobile }) => {
@@ -18,17 +22,23 @@ const StoryPage = ({ path, changePath, move, showing, isMobile }) => {
       <StoryHeader path={path} changePath={changePath} />
       <div className="content">
         <FullPageImageAndText img={EngagementPic} opacity="0.4" color="#B07FCD">
-          It began on the same day one year apart
+          On the same day, a year apart
         </FullPageImageAndText>
         <BabyPics move={move} />
+        <StorySubtitle>
+          Both of us got our start
+        </StorySubtitle>
         <FullPageImageAndText img={BaldKiss} color="white" imgPosition="center 30%">
           A bit later on we met in college
         </FullPageImageAndText>
-        <FullPageImageAndText img={NerdSmile} color="white" imgPosition="center 30%" {...(isMobile ? {} : {imgSize: '50%'})}>
+        <FullPageImageAndText img={NerdsSmile} color="white" imgPosition="center 30%" {...(isMobile ? {} : {imgSize: '50%'})}>
           We went to some parties
         </FullPageImageAndText>
-        <FullPageImageAndText img={StarvedRock} color="white" imgPosition="center 30%" imgSize="50%">
-          Went on some trips
+        <FullPageImageAndText img={StarvedRock} color="white" imgPosition="center 30%" {...(isMobile ? {} : {imgSize: '50%'})}>
+          And went on some trips
+        </FullPageImageAndText>
+        <FullPageImageAndText img={SmileAtHome} color="white" imgPosition="center 30%" {...(isMobile ? {} : {imgSize: '50%'})}>
+          Falling in love all the while
         </FullPageImageAndText>
         <EngagementVideo showing={move || showing} />
       </div>
